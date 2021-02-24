7c3e328e80a8517c1731f90baabdaca8c0213e46
Hasan Shobri <hasan.shobri@outlook.com>
2021-02-24 22:57:17 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DashboardBNI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
